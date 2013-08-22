30067120c90776c69a3f3c6b5ec4a427489bd39e
Alfred Jayaprakash <alfred.jayaprakash@nus.edu.sg>
2013-08-22 15:23:48 +0800

    Implemented Logout and other UI improvements

--- view/src/main/java/com/media2359/euphoria/view/client/main/MainPanel.java
+++ view/src/main/java/com/media2359/euphoria/view/client/main/MainPanel.java
@@ -1,11 +1,18 @@
 package com.media2359.euphoria.view.client.main;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.Composite;
+import com.sencha.gxt.widget.core.client.Dialog.PredefinedButton;
+import com.sencha.gxt.widget.core.client.box.ConfirmMessageBox;
+import com.sencha.gxt.widget.core.client.event.HideEvent;
+import com.sencha.gxt.widget.core.client.event.HideEvent.HideHandler;
 
 public class MainPanel extends Composite {
 	interface MyUiBinder extends UiBinder<Widget, MainPanel> {
@@ -29,4 +36,20 @@ public class MainPanel extends Composite {
 	public static native String getUsername()/*-{
     	return $wnd.getLoginUsername();
 	}-*/; 
+	
+	@UiHandler("logoutButton")
+	public void handleLogout(ClickEvent clickEvent) {
+		final ConfirmMessageBox confirm = new ConfirmMessageBox("Confirmation","Are you sure you want to logout?");
+		confirm.addHideHandler(new HideHandler() {
+		      public void onHide(HideEvent event) {
+		          if (confirm.getHideButton() == confirm.getButtonById(PredefinedButton.YES.name())) {
+		            // Upon YES, redirect to logout
+		        	Window.open("/jsp/login/logout.jsp", "_top", null);
+		          } else if (confirm.getHideButton() == confirm.getButtonById(PredefinedButton.NO.name())){
+		            // perform NO action
+		          }
+		        }
+		      });
+		confirm.show();
+	}
 }
