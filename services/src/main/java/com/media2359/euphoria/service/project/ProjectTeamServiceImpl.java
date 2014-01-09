75e066b608ac5caabb91df3f995bdc134df4a331
Shiv Kole <shiv.kole@gmail.com>
2014-01-09 22:02:11 +0800

    Project Team Service Correction

--- services/src/main/java/com/media2359/euphoria/service/project/ProjectTeamServiceImpl.java
+++ services/src/main/java/com/media2359/euphoria/service/project/ProjectTeamServiceImpl.java
@@ -1,12 +1,16 @@
 package com.media2359.euphoria.service.project;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.media2359.euphoria.dao.project.ProjectTeamDAO;
+import com.media2359.euphoria.model.project.Project;
 import com.media2359.euphoria.model.project.ProjectTeam;
+import com.media2359.euphoria.view.dto.project.ProjectDTO;
 import com.media2359.euphoria.view.dto.project.ProjectTeamDTO;
 import com.media2359.euphoria.view.server.project.ProjectTeamService;
 
@@ -16,21 +20,39 @@ public class ProjectTeamServiceImpl implements ProjectTeamService {
 
 	@Autowired
 	private ProjectTeamDAO projectTeamDao;
+	
 	@Override
 	public ProjectTeamDTO getProjectTeam(Integer projectKey) {
-		return projectTeamDao.getProjectTeam(projectKey).prepareProjectTeamDTO();
+		Project project = new Project();
+		project.setId(projectKey);
+		return projectTeamDao.getProjectTeam(project).prepareProjectTeamDTO();
 	}
 
 	
 	@Override
 	public String submitProjectTeam(ProjectTeamDTO projectTeamDto)  {
 		try{
-			ProjectTeam projectTeam = projectTeamDao.getProjectTeam(projectTeamDto.getProjectTeamKey());
+			ProjectTeam projectTeam = projectTeamDao.getProjectTeam(projectTeamDto.getProject());
 			if(projectTeam!=null){
-				projectTeamDao.updateProjectTeam(projectTeamDto.prepareProjectTeam());
+				projectTeam.setProject(projectTeamDto.getProject());
+				projectTeam.setProjectManagers(projectTeamDto.getProjectManagers());
+				projectTeam.setProjectTeamKey(projectTeamDto.getProjectTeamKey());
+				projectTeam.setProjectTeamName(projectTeamDto.getProjectTeamName());
+				projectTeam.setTeamMembers(projectTeamDto.getTeamMembers());
+				projectTeam.setCreatedBy(projectTeamDto.getCreatedBy());
+				projectTeam.setCreatedTstmp(new Date());
+				projectTeamDao.updateProjectTeam(projectTeam);
 				
 			}else{
-				projectTeamDao.addProjectTeam(projectTeamDto.prepareProjectTeam());
+				projectTeam = new ProjectTeam();
+				projectTeam.setProject(projectTeamDto.getProject());
+				projectTeam.setProjectManagers(projectTeamDto.getProjectManagers());
+				projectTeam.setProjectTeamKey(projectTeamDto.getProjectTeamKey());
+				projectTeam.setProjectTeamName(projectTeamDto.getProjectTeamName());
+				projectTeam.setTeamMembers(projectTeamDto.getTeamMembers());
+				projectTeam.setCreatedBy(projectTeamDto.getCreatedBy());
+				projectTeam.setCreatedTstmp(new Date());
+				projectTeamDao.addProjectTeam(projectTeam);
 			}
 			return "SUCCESS";
 		}catch(Exception exp){
