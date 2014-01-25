bc64df57603a664926f7f09fff5176730b09beb5
alfred-jayaprakash <alfredisfun@gmail.com>
2014-01-25 11:44:02 +0800

    Added email support

--- services/src/test/java/com/media2359/euphoria/service/email/EmailServiceTest.java
+++ services/src/test/java/com/media2359/euphoria/service/email/EmailServiceTest.java
@@ -1,54 +1,27 @@
 package com.media2359.euphoria.service.email;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.mail.SimpleMailMessage;
-
-import com.ibm.icu.impl.Assert;
-import com.postmark.PostmarkMailSender;
 
 public class EmailServiceTest {
-	EmailService service;
+	EmailServiceImpl service;
 
 	static final String TEST_API_KEY = "aa239d6c-c104-4262-b1d1-5c809fd818c4";
 
-	static final String VALID_EMAIL = "info@postmark.com";
+	static final String VALID_EMAIL = "a0079972@nus.edu.sg";
 	static final String INVALID_EMAIL = "test-exemple.com";
 
 	@Before
 	public void initialize() {
-		service = new EmailService();
-	}
-
-	public void testSendEmail() {
-		try {
-			service.sendEmail("alfredisfun@gmail.com", "Test subject",
-					"This is a test message");
-		} catch (AddressException e) {
-			Assert.fail(e);
-		} catch (MessagingException e) {
-			Assert.fail(e);
-		}
+		
+		service = new EmailServiceImpl();
+		service.setPostMarkApiKey(TEST_API_KEY);
+		service.setSenderEmail(VALID_EMAIL);
 	}
 
 	@Test
-	public void testSendEmailPostmark() {
-
-		PostmarkMailSender mailSender = new PostmarkMailSender(TEST_API_KEY);
-		
-		Logger.getLogger("com.postmark").setLevel(Level.FINEST);
-
-		SimpleMailMessage m = new SimpleMailMessage();
-		m.setFrom("alfredisfun@gmail.com");
-		m.setTo("alfredisfun@gmail.com");
-		m.setSubject("Test Mail");
-		m.setText("This is the body");
-		mailSender.send(m);
+	public void testSendValidEmail() {
+		service.sendEmail(new String[]{"alfredisfun@gmail.com"}, "Test subject",
+					"This is a test message");
 	}
 }
